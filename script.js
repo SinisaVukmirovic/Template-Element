292bb2cefc701b82a0fa267d09abc1be87c11da5
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-01-29 23:58:11 +0100

    Search functionality

--- script.js
+++ script.js
@@ -1,20 +1,37 @@
 const APIurl = 'https://jsonplaceholder.typicode.com/users';
 
-const userCardTemplate = document.querySelector('[data-usercard-template]');
+const searchInput = document.querySelector('[data-search-input]');
 const userCardsContainer = document.querySelector('[data-user-cards-container]');
+const userCardTemplate = document.querySelector('[data-usercard-template]');
+
+let users = [];
 
 fetch(APIurl)
     .then(res => res.json())
     .then(data => {
-        data.forEach(user => {
+        users = data.map(user => {
             const card = userCardTemplate.content.cloneNode(true).children[0];
 
             const cardHeader = card.querySelector('[data-card-header]');
             const cardBody = card.querySelector('[data-card-body]');
 
             cardHeader.textContent = user.name;
-            cardBody.textContent = user.name;
+            cardBody.textContent = user.email;
 
             userCardsContainer.append(card);
+
+            return { name: user.name, email: user.email, element: card }
         });
     });
+
+const searchUsers = (e) => {
+    const value = e.target.value.toLowerCase();
+    
+    users.forEach(user => {
+        const isVisible = user.name.toLowerCase().includes(value) || user.email.toLowerCase().includes(value);
+
+        user.element.classList.toggle('hide', !isVisible);
+    });
+}
+
+searchInput.addEventListener('input', searchUsers);
